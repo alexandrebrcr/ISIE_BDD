4c4ea2bf89fae7e0c243410c9bd5db1a658716c5
Alexandre BOURCIER <alexandre.bourcier989@gmail.com>
2024-03-21 17:30:19 +0100

    Create 2024_BDD_BOURCIER_ALEXANDRE_EX1_1.sql

---
+++ 2024_BDD_BOURCIER_ALEXANDRE_EX1/2024_BDD_BOURCIER_ALEXANDRE_EX1_1/2024_BDD_BOURCIER_ALEXANDRE_EX1_1.sql
@@ -0,0 +1,20 @@
+INSERT INTO Utilisateurs (NumeroEtudiant, Nom, Prenom, Email)
+VALUES (123456, 'Dupont', 'Jean', 'jean.dupont@etu.univ-tours.fr');
+INSERT INTO Utilisateurs (NumeroEtudiant, Nom, Prenom, Email)
+VALUES (123457, 'Mino', 'Christophe', 'christophe.mino@etu.univ-tours.fr');
+INSERT INTO Utilisateurs (NumeroEtudiant, Nom, Prenom, Email)
+VALUES (123458, 'Chicho', 'Marc', 'marc.chicho@etu.univ-tours.fr');
+INSERT INTO Utilisateurs (NumeroEtudiant, Nom, Prenom, Email)
+VALUES (123459, 'Tuto', 'Fréd', 'fred.tuto@etu.univ-tours.fr');
+INSERT INTO Utilisateurs (NumeroEtudiant, Nom, Prenom, Email)
+VALUES (123460, 'Bourcier', 'Alex', 'alex.bourcier@etu.univ-tours.fr');
+INSERT INTO Utilisateurs (NumeroEtudiant, Nom, Prenom, Email)
+VALUES (123461, 'test', 'toto', 'toto.test@etu.univ-tours.fr');
+INSERT INTO Utilisateurs (NumeroEtudiant, Nom, Prenom, Email)
+VALUES (123462, 'Yvan', 'Yvan', 'yvan.yvan@etu.univ-tours.fr');
+INSERT INTO Utilisateurs (NumeroEtudiant, Nom, Prenom, Email)
+VALUES (123463, 'Rido', 'Charlotte', 'rido.charlotte@etu.univ-tours.fr');
+INSERT INTO Utilisateurs (NumeroEtudiant, Nom, Prenom, Email)
+VALUES (123464, 'Filo', 'Juliette', 'filo.juliette@etu.univ-tours.fr');
+INSERT INTO Utilisateurs (NumeroEtudiant, Nom, Prenom, Email)
+VALUES (123465, 'Lemoine', 'Lilou', 'lemoine.lilou@etu.univ-tours.fr');
